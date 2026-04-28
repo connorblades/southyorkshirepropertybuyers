460058b2ad5d1a1e92dfc4a9c26eb97f1322ef97
Connor Blades <connor@nura.solutions>
2026-04-28 22:58:38 +0100

    Add www to non-www 301 redirect

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const PORT = process.env.PORT || 3000;
 const GHL_WEBHOOK = 'https://services.leadconnectorhq.com/hooks/Lt1hJmen7QrCZZJy7bzf/webhook-trigger/0e1dc81a-9083-4e12-be50-a0aa1d486a27';
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  if (req.headers.host && req.headers.host.startsWith('www.')) {
+    return res.redirect(301, 'https://southyorkshirepropertybuyers.com' + req.url);
+  }
+  next();
+});
+
 app.use(express.static(path.join(__dirname)));
 
 app.post('/api/submit', async (req, res) => {
